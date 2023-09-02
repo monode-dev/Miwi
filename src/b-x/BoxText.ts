4657eff3276a1d08c71168cc4cabed9e3b551511
Melchiah Mauck <MelchiahMauck@gmail.com>
2023-09-01 20:04:47 -0400

    Publishing 0.0.109

--- src/b-x/BoxText.ts
+++ src/b-x/BoxText.ts
@@ -1,14 +1,14 @@
-import { isNum, exists, CssProps } from "./BoxUtils";
-import { Align, AlignSingleAxis, Overflow, _FlexAlign } from "./BoxLayout";
-import { sizeToCss } from "./BoxSize";
+import { isNum, exists, CssProps } from './BoxUtils'
+import { Align, AlignSingleAxis, Overflow, _FlexAlign } from './BoxLayout'
+import { sizeToCss } from './BoxSize'
 
 export type TextSty = {
-  scale: number | string;
-  textColor: string;
+  scale: number | string
+  textColor: string
   // NOTE: Eventually we might want to make this a number so it can be granularly controlled. With presets for thin, normal, and bold.
-  textIsBold: boolean;
-  textIsItalic: boolean;
-  textIsUnderlined: boolean;
+  textIsBold: boolean
+  textIsItalic: boolean
+  textIsUnderlined: boolean
   /** NOTE: Flex container treats its children as flex items, and unfortunately, the
    * text-overflow: ellipsis; doesn't apply to flex items. If you want to keep using
    * flex for other properties, a solution is to wrap your text in an inner div (or
@@ -23,12 +23,12 @@ export type TextSty = {
    * This should give you the desired behavior: text that is too long to fit inside
    * the inner div will be truncated and an ellipsis will be added at the end. */
   // useEllipsisForOverflow: boolean;
-};
+}
 
 // const fontSizeToHtmlUnit = 0.9;
 export function numToFontSize(num: number) {
   // return sizeToCss(fontSizeToHtmlUnit * num);
-  return sizeToCss(num); // * 1.3);
+  return sizeToCss(num) // * 1.3);
 }
 
 export function computeTextStyle(
@@ -38,18 +38,10 @@ export function computeTextStyle(
 ): CssProps {
   return {
     // Text Style
-    fontFamily: `Roboto`, //sty.fontFamily ?? `Roboto` ?? `inherit`,
+    fontFamily: `inherit`, //`Roboto`,
     fontSize: isNum(sty.scale) ? numToFontSize(sty.scale) : sty.scale,
-    fontWeight: exists(sty.textIsBold)
-      ? sty.textIsBold
-        ? `bold`
-        : `normal`
-      : undefined,
-    fontStyle: exists(sty.textIsItalic)
-      ? sty.textIsItalic
-        ? `italic`
-        : `normal`
-      : undefined,
+    fontWeight: exists(sty.textIsBold) ? (sty.textIsBold ? `bold` : `normal`) : undefined,
+    fontStyle: exists(sty.textIsItalic) ? (sty.textIsItalic ? `italic` : `normal`) : undefined,
     textDecoration: exists(sty.textIsUnderlined)
       ? sty.textIsUnderlined
         ? `underline`
@@ -63,11 +55,12 @@ export function computeTextStyle(
         : // We assume for now that all other aligns cam be treated as center
           `center`,
     lineHeight: sty.scale === undefined ? undefined : sizeToCss(sty.scale),
-    whiteSpace: // whiteSapce casacdes, so we need to explicity set it.
+    // whiteSapce casacdes, so we need to explicity set it.
+    whiteSpace:
       overflowX === Overflow.crop || overflowX === Overflow.forceStretchParent
         ? `nowrap`
         : `normal`,
     // textOverflow: sty.useEllipsisForOverflow ?? false ? `ellipsis` : undefined,
     color: sty.textColor,
-  };
+  }
 }
