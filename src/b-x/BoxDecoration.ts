59239b970a0d1774d9ab820227d8ddc1e208d200
Melchiah Mauck <MelchiahMauck@gmail.com>
2023-11-02 17:36:30 -0700

    Publishing 0.0.182

--- src/b-x/BoxDecoration.ts
+++ src/b-x/BoxDecoration.ts
@@ -57,7 +57,7 @@ export const decorationStyler = baseStyler.addStyler<DecorationSty>((sty, htmlEl
   const backgroundIsImage =
     (sty.background?.startsWith(`data:image`) || sty.background?.startsWith(`/`)) ?? false
   htmlElement.style.backgroundColor = backgroundIsImage ? `` : sty.background ?? ``
-  console.log((sty as any).shouldLog)
+  console.log(sty)
   if ((sty as any).shouldLog) {
     console.log(`sty.background`, sty.background)
     console.log(`htmlElement.style.backgroundColor`, htmlElement.style.backgroundColor)
