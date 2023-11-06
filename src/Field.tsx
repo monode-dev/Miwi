f0dbdfae0f71222eda214edc9ac4c382c5e5a62a
Melchiah Mauck <MelchiahMauck@gmail.com>
2023-11-06 12:23:25 -0800

    Publishing 0.0.221

--- src/Field.tsx
+++ src/Field.tsx
@@ -197,6 +197,7 @@ export function Field(
     }
     return lineCount > 1 ? <textarea {...inputProps} /> : <input {...inputProps} />
   }
+  console.log(props.onClick)
   return (
     <Row
       background="red"
