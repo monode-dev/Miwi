03828ddf0f5dcce30601740936a486cf1fd5c500
Melchiah Mauck <MelchiahMauck@gmail.com>
2023-11-13 18:50:57 -0800

    Publishing 0.0.328

--- src/Box/Box.tsx
+++ src/Box/Box.tsx
@@ -1,4 +1,4 @@
-import { onMount, type JSX, type ParentProps, onCleanup } from 'solid-js'
+import { type JSX, type ParentProps, onCleanup, onMount } from 'solid-js'
 import { Sig, exists, sig, watchEffect } from '../utils'
 import { makePropParser, observeElement } from './BoxUtils'
 import { _FlexAlign, watchBoxLayout, LayoutSty, stackClassName, Axis } from './BoxLayout'
@@ -24,43 +24,45 @@ export function Box(props: BoxProps) {
   const element = sig<HTMLElement | undefined>(undefined)
   const parseProp: (...args: any[]) => any = makePropParser(props)
 
-  // Observe Parent
-  const {
-    parentAxis,
-    parentPaddingLeft,
-    parentPaddingTop,
-    parentPaddingRight,
-    parentPaddingBottom,
-  } = _watchParent(element)
+  onMount(() => {
+    // Observe Parent
+    const {
+      parentAxis,
+      parentPaddingLeft,
+      parentPaddingTop,
+      parentPaddingRight,
+      parentPaddingBottom,
+    } = _watchParent(element)
 
-  // Observe Children
-  const { hasMoreThanOneChild, aChildsWidthGrows, aChildsHeightGrows } = _watchChildren(element)
+    // Observe Children
+    const { hasMoreThanOneChild, aChildsWidthGrows, aChildsHeightGrows } = _watchChildren(element)
 
-  // Compute Layout
-  const { alignX, overflowX } = watchBoxLayout(parseProp, element, { hasMoreThanOneChild })
+    // Compute Layout
+    const { alignX, overflowX } = watchBoxLayout(parseProp, element, { hasMoreThanOneChild })
 
-  // Compute Size
-  watchBoxSize(parseProp, element, {
-    parentAxis,
-    parentPaddingLeft,
-    parentPaddingRight,
-    parentPaddingTop,
-    parentPaddingBottom,
-    aChildsWidthGrows,
-    aChildsHeightGrows,
+    // Compute Size
+    watchBoxSize(parseProp, element, {
+      parentAxis,
+      parentPaddingLeft,
+      parentPaddingRight,
+      parentPaddingTop,
+      parentPaddingBottom,
+      aChildsWidthGrows,
+      aChildsHeightGrows,
+    })
+
+    // Compute Decoration
+    watchBoxDecoration(parseProp, element)
+
+    // Compute Text Styling
+    watchBoxText(parseProp, element, {
+      alignX,
+      overflowX,
+    })
+
+    // Computer Interactivity
+    watchBoxInteraction(parseProp, element)
   })
-
-  // Compute Decoration
-  watchBoxDecoration(parseProp, element)
-
-  // Compute Text Styling
-  watchBoxText(parseProp, element, {
-    alignX,
-    overflowX,
-  })
-
-  // Computer Interactivity
-  watchBoxInteraction(parseProp, element)
 
   // TODO: Toggle element type based on "tag" prop.
   return (
@@ -94,7 +96,7 @@ function _watchParent(element: Sig<HTMLElement | undefined>) {
         attributeFilter: [`style`, `class`],
       },
       () => {
-        const parentElement = element.value!.parentElement
+        const parentElement = (element.value as any).parentElement
         if (!exists(parentElement)) return
         const parentStyle = getComputedStyle(parentElement)
         // Parent Axis
@@ -144,7 +146,7 @@ function _watchChildren(element: Sig<HTMLElement | undefined>) {
   const hasMoreThanOneChild = sig(false)
   const activeChildObservers: MutationObserver[] = []
   const childListObserver = observeElement(
-    element.value!,
+    element.value as any,
     {
       childList: true,
     },
