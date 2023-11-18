c5c372de0ec5606f963e4a2a0a2610d9d2a441a6
MechMel <melchiahmauck@gmail.com>
2023-11-17 17:56:36 -0800

    Publishing 0.0.373

--- src/Box/BoxInteraction.ts
+++ src/Box/BoxInteraction.ts
@@ -16,19 +16,19 @@ export type InteractionSty = Partial<{
 }>
 
 const bonusTouchAreaClassName = `miwi-bonus-touch-area`
-// const style = document.createElement(`style`)
-// style.textContent = `
-// .${bonusTouchAreaClassName}::before {
-//   content: '';
-//   position: absolute;
-//   top: -0.5rem;
-//   right: -0.5rem;
-//   bottom: -0.5rem;
-//   left: -0.5rem;
-// }
-// `
-// // z-index: -1;
-// document.body.appendChild(style)
+const style = document.createElement(`style`)
+style.textContent = `
+.${bonusTouchAreaClassName}::before {
+  content: '';
+  position: absolute;
+  top: -0.5rem;
+  right: -0.5rem;
+  bottom: -0.5rem;
+  left: -0.5rem;
+  z-index: 2;
+}
+`
+document.body.appendChild(style)
 
 export function watchBoxInteraction(
   parseProp: ParseProp<InteractionSty>,
