79e2f9d633cff307257e3259716d9526f20d3391
MechMel <melchiahmauck@gmail.com>
2023-12-22 13:53:53 -0800

    Publishing 0.0.451

--- src/TabView.tsx
+++ src/TabView.tsx
@@ -85,6 +85,7 @@ export function TabView(
       heightGrows
       alignTopCenter
       overflowX={$Overflow.crop}
+      overflowY={$Overflow.crop}
       overrideProps={props}
     >
       <Box widthGrows heightGrows overflowX={$Overflow.crop}>
