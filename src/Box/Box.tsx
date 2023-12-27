fb6b9dfa6a01c684aed026ca7dc8e8c897f90088
MechMel <melchiahmauck@gmail.com>
2023-12-26 23:09:18 -0800

    Publishing 0.0.533

--- src/Box/Box.tsx
+++ src/Box/Box.tsx
@@ -98,12 +98,6 @@ export function Box(props: BoxProps) {
 
     // Computer Interactivity
     watchBoxInteraction(parseProp, element, { isScrollable });
-
-    // The element is set up so we can give it to anyone who asked for it now
-    parseProp(`getElement`, true).forEach((getter: any) => {
-      if (typeof getter !== `function`) return;
-      getter(element.value);
-    });
   });
 
   // TODO: Toggle element type based on "tag" prop.
@@ -119,10 +113,10 @@ export function Box(props: BoxProps) {
       ref={el => {
         element.value = el;
         // Notify element getters
-        // parseProp(`getElement`, true).forEach((getter: any) => {
-        //   if (typeof getter !== `function`) return;
-        //   getter(element.value);
-        // });
+        parseProp(`getElement`, true).forEach((getter: any) => {
+          if (typeof getter !== `function`) return;
+          getter(element.value);
+        });
       }}
     />
   );
