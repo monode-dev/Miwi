f79a3436125be4082842432ef152562a4660d328
MechMel <15083231+MechMel@users.noreply.github.com>
2024-05-31 12:45:33 -0700

    Publishing 0.0.696

--- src/Label.tsx
+++ src/Label.tsx
@@ -1,7 +1,9 @@
-import { BoxProps } from "./Box/Box";
+import { Box, BoxProps } from "./Box/Box";
 import { useFormula, exists } from "./utils";
 import { Row } from "./Row";
 import { Txt } from "./Txt";
+import { Match, Switch } from "solid-js";
+import { Icon } from "./Icon";
 
 export function Label(
   props: {
@@ -15,15 +17,25 @@ export function Label(
   const shouldShowLabel = useFormula(() => exists(props.label) && props.label.length > 0);
 
   return shouldShowLabel.value ? (
-    <Row widthGrows padBetween={0.25} alignTopLeft overrideProps={props}>
-      <Txt
-        singleLine
-        hint={props.hint ?? false}
-        width={props.labelWidth ?? `fit-content`}
-        fill={props.labelBackground}
-      >
-        {props.label}:
-      </Txt>
+    <Row
+      widthGrows
+      padBetween={0.25}
+      alignTopLeft
+      stroke={props.hint ? $theme.colors.hint : undefined}
+      overrideProps={props}
+    >
+      <Switch>
+        <Match when={exists(props.icon)}>
+          <Box fill={props.labelBackground}>
+            <Icon iconPath={props.icon!} scale={props.labelWidth ?? undefined} />
+          </Box>
+        </Match>
+        <Match when={exists(props.label)}>
+          <Txt singleLine width={props.labelWidth ?? `fit-content`} fill={props.labelBackground}>
+            {props.label}:
+          </Txt>
+        </Match>
+      </Switch>
       {props.children}
     </Row>
   ) : (
