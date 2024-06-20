97604cba64426adb3a2e233115ca43eb8a8d6605
MechMel <15083231+MechMel@users.noreply.github.com>
2024-06-20 16:32:44 -0700

    Publishing 0.0.719

--- src/Field.tsx
+++ src/Field.tsx
@@ -234,8 +234,7 @@ export function Field(
       onKeyPress: handleKeyPress,
       onPaste: handlePaste,
       class: "field",
-      // TODO: 100 -> undefined
-      rows: maxLines.value === Infinity ? 100 : maxLines.value,
+      rows: maxLines.value === Infinity ? undefined : maxLines.value,
       wrap: maxLines.value > 1 ? (`soft` as const) : undefined,
       ["auto-capitalize"]: props.capitalize ?? "none",
       enterkeyhint: props.enterKeyHint ?? `done`,
