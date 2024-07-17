07c5160d8da320bf8cdeb6a78d3bf6ead4bc4d5e
MechMel <15083231+MechMel@users.noreply.github.com>
2024-07-17 13:37:18 -0700

    Publishing 0.0.728

--- src/theme.ts
+++ src/theme.ts
@@ -2,21 +2,21 @@ export const sizeScaleCssVarName = `--miwi-size-scale`;
 
 export function cssVarForMiwiTexture(nameInPalette: string, color: string) {
   return {
-    key: `--miwi-texture-${nameInPalette}`,
+    key: `--miwi-color-${nameInPalette}`,
     value: color,
   };
 }
 
 export const defaultTheme = {
   palette: {
-    primary: `var(--miwi-texture-primary)`,
-    accent: `var(--miwi-texture-accent)`,
-    pageBackground: `var(--miwi-texture-page-fill)`,
-    text: `var(--miwi-texture-text)`,
-    hint: `var(--miwi-texture-hint)`,
-    lightHint: `var(--miwi-texture-light-hint)`,
-    warning: `var(--miwi-texture-warning)`,
-    error: `var(--miwi-texture-error)`,
+    primary: `var(--miwi-color-primary)`,
+    accent: `var(--miwi-color-accent)`,
+    pageBackground: `var(--miwi-color-page-fill)`,
+    text: `var(--miwi-color-text)`,
+    hint: `var(--miwi-color-hint)`,
+    lightHint: `var(--miwi-color-light-hint)`,
+    warning: `var(--miwi-color-warning)`,
+    error: `var(--miwi-color-error)`,
   },
 };
 
@@ -36,13 +36,13 @@ export type MiwiTheme = {
 export const strokeTexture = `currentColor`;
 export const theme = {
   palette: {
-    primary: `var(--miwi-texture-primary)`,
-    accent: `var(--miwi-texture-accent)`,
-    pageBackground: `var(--miwi-texture-page-fill)`,
-    text: `var(--miwi-texture-text)`,
-    hint: `var(--miwi-texture-hint)`,
-    lightHint: `var(--miwi-texture-light-hint)`,
-    warning: `var(--miwi-texture-warning)`,
-    error: `var(--miwi-texture-error)`,
+    primary: `var(--miwi-color-primary)`,
+    accent: `var(--miwi-color-accent)`,
+    pageBackground: `var(--miwi-color-page-fill)`,
+    text: `var(--miwi-color-text)`,
+    hint: `var(--miwi-color-hint)`,
+    lightHint: `var(--miwi-color-light-hint)`,
+    warning: `var(--miwi-color-warning)`,
+    error: `var(--miwi-color-error)`,
   },
 };
