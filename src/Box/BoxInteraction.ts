b4b21a93a5759d18bd440fefab32971bea667931
MechMel <15083231+MechMel@users.noreply.github.com>
2024-09-03 18:11:11 -0700

    Publishing 0.2.38

--- src/Box/BoxInteraction.ts
+++ src/Box/BoxInteraction.ts
@@ -19,30 +19,6 @@ export type InteractionSty = Partial<{
   onclick: (e: MouseEvent) => void;
 }>;
 
-// const bonusTouchAreaClassName = `miwi-bonus-touch-area`;
-// const style = document.createElement(`style`);
-// TODO: Use bonus touch radius to control the size of the touch area.
-/* We tried to prevent clip from affecting touch radius since that seemed like a good
- * idea, but clip needs to effect touch radius to some degree. Imagine a scrollable
- * body, withe a bottom nav bar. Components under the bottom nav bar should not be
- * interactable. */
-// Use background: rgba(255, 0, 0, 0.125); for debugging.
-// style.textContent = `
-// :root {
-//   --miwi-bonus-touch-radius: -0.5rem
-// }
-// .${bonusTouchAreaClassName}::before {
-//   content: '';
-//   position: absolute;
-//   top: var(--miwi-bonus-touch-radius);
-//   right: var(--miwi-bonus-touch-radius);
-//   bottom: var(--miwi-bonus-touch-radius);
-//   left: var(--miwi-bonus-touch-radius);
-//   pointer-events: auto;
-//   background: rgba(255, 0, 0, 0.125);
-// }`; //z-index: -1;
-// document.body.appendChild(style);
-
 export function watchBoxInteraction(
   parseProp: ParseProp<InteractionSty>,
   element: Prop<HTMLElement | undefined>,
@@ -72,16 +48,6 @@ export function watchBoxInteraction(
         ? (e: MouseEvent) => onClickListeners.forEach(listener => listener(e))
         : null;
     element.value.style.cursor = parseProp(`cssCursor`) ?? (isClickable ? `pointer` : `default`);
-    // NOTE: This seems to be adding extra padding to layouts when a full-screen dialog and keyboard are open.
-    // element.value.classList.toggle(
-    //   bonusTouchAreaClassName,
-    //   parseProp(`bonusTouchArea`) ?? isClickable,
-    // );
-    /* We previously considered using a `::before` pseudo element with position `absolute, but this was affecting
-     * layout, and overflow clip on a box was clipping that box's touch radius. When we did that, we tried to prevent
-     * clip from affecting touch radius since that seemed like a good idea, but clip needs to effect touch radius to
-     * some degree. Imagine a scrollable body, withe a bottom nav bar. Components under the bottom nav bar should not
-     * be interactable. */
     // Use color `rgba(255, 0, 0, 0.125)` for debugging.
     const touchRadiusCss = doNow(() => {
       const touchRadius = parseProp(`touchRadius`);
@@ -93,12 +59,36 @@ export function watchBoxInteraction(
             ? undefined
             : muToCss(touchRadius)
         : isClickable
-          ? 2
+          ? muToCss(2)
           : undefined;
     });
     element.value.style.outline = exists(touchRadiusCss)
       ? `${touchRadiusCss} solid rgba(255, 0, 0, 0.125)`
       : ``;
+    /* Previously to get touch radius we used a `::before` pseudo element with position `absolute, but this was adding
+     * extra padding to layouts when both a full-screen dialog and keyboard where open, and overflow clip on a box was
+     * clipping that box's touch radius. When we did that, we tried to prevent clip from affecting touch radius since
+     * that seemed like a good idea, but clip needs to effect touch radius to some degree. Imagine a scrollable body,
+     * with a bottom nav bar. Components under the bottom nav bar should not be interactable. */
+    // const bonusTouchAreaClassName = `miwi-bonus-touch-area`;
+    // element.value.classList.toggle(
+    //   bonusTouchAreaClassName,
+    //   parseProp(`bonusTouchArea`) ?? isClickable,
+    // );
+    // style.textContent = `
+    // :root {
+    //   --miwi-bonus-touch-radius: -0.5rem
+    // }
+    // .${bonusTouchAreaClassName}::before {
+    //   content: '';
+    //   position: absolute;
+    //   top: var(--miwi-bonus-touch-radius);
+    //   right: var(--miwi-bonus-touch-radius);
+    //   bottom: var(--miwi-bonus-touch-radius);
+    //   left: var(--miwi-bonus-touch-radius);
+    //   pointer-events: auto;
+    //   background: rgba(255, 0, 0, 0.125);
+    // }`; //z-index: -1;
   });
 
   // On Mouse Enter
