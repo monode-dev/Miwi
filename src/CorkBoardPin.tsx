0f334fc4a606df7e0f7276e2d87548fc0e359316
fileman42 <fileman.mauck@gmail.com>
2024-09-25 09:31:51 -0700

    Slight edits to CorkBoardPin

--- src/CorkBoardPin.tsx
+++ src/CorkBoardPin.tsx
@@ -1,11 +1,11 @@
 import { prop } from "mufasa";
 import { Box, useProp } from "./miwi";
 
-export function CorkBoardPin(props: { children: any, posX?: number, posY?: number }) {
-  const posX = useProp(props.posX ?? 0);
-  const posY = useProp(props.posY ?? 0);
+export function CorkBoardPin(props: { children: any, posX?: number, posY?: number}) {
+  const posX = prop(props.posX ?? 0);
+  const posY = prop(props.posY ?? 0);
   return (
-    <Box 
+    <Box
       data-corkboard-pin
       cssStyle={{ position: "absolute", left: `${posX}px`, top: `${posY}px` }}
     >
